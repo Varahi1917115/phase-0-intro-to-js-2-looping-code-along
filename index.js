f4b978c2cfa362a220859cc4f2aff4aa4aeaef05
Samarth <Varahi1917115@gmail.com>
2022-04-11 10:45:06 -0400

    commit tree 93c58e3801c5ab62d12e93daea73406f5e0f583b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+
+
+
+function countDown() {
+    let countDown = 10;
+        while (countDown > -1) {
+            console.log(countDown--);
+
+        }
+        return countDown;
+}
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "surprise"))
